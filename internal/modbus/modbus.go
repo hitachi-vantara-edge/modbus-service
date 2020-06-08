c96d793eb0623b1ede24a2e13a8a44e290e17dc6
Eddy Kuwana <eddy.kuwana@hitachivantara.com>
2020-06-07 17:10:32 -0700

    manage multiple connections to modbus devices

--- internal/modbus/modbus.go
+++ internal/modbus/modbus.go
@@ -2,6 +2,7 @@ package modbus
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -18,7 +19,7 @@ import (
 const gRPCServerPort = 10000
 
 type modbusServer struct {
-	connection *modbusConnection
+	connections map[string]*modbusConnection
 }
 
 type modbusConnection struct {
@@ -31,72 +32,85 @@ type modbusConnection struct {
 }
 
 func NewModbusServer(addr string, port int) *modbusServer {
-	return &modbusServer{connection: &modbusConnection{}}
+	return &modbusServer{connections: map[string]*modbusConnection{}}
 }
 
 func (s *modbusServer) ReadReg(ctx context.Context, req *modbus.ReadRegRequest) (*modbus.
 	ReadRegReply, error) {
-	var ret int32
-	var data = make([]uint16, 65535)
-	ret = libmodbus.SetSlave(s.connection.Context, int32(req.SlaveAddr))
-	if ret == -1 {
-		return nil, fmt.Errorf("fail to set slave addr. ret=%v", ret)
-	}
-	if req.ReadInput {
-		ret = libmodbus.ReadInputRegisters(s.connection.Context, req.StartAddr, req.NumOfReg, data)
-	} else {
-		ret = libmodbus.ReadRegisters(s.connection.Context, req.StartAddr, req.NumOfReg, data)
-	}
-	if ret == -1 {
-		return nil, fmt.Errorf("fail to read register. ret=%v", ret)
-	}
-
-	values := make([]*modbus.ReadRegReply_RegisterValue, 0)
-	for i, v := range data {
-		if i >= int(req.NumOfReg) {
-			break
+	if connection, ok := s.connections[req.ConnectionId]; ok {
+		var ret int32
+		var data = make([]uint16, 65535)
+		ret = libmodbus.SetSlave(connection.Context, int32(req.SlaveAddr))
+		if ret == -1 {
+			return nil, fmt.Errorf("fail to set slave addr. ret=%v", ret)
 		}
-		addr := i + int(req.StartAddr)
-		log.Debug().Msgf("reg[%v]=%v (0x%X)\n", addr, v, v)
-		values = append(values, &modbus.ReadRegReply_RegisterValue{
-			Addr:  int32(addr),
-			Value: fmt.Sprint(v),
-		})
+		if req.ReadInput {
+			ret = libmodbus.ReadInputRegisters(connection.Context, req.StartAddr, req.NumOfReg, data)
+		} else {
+			ret = libmodbus.ReadRegisters(connection.Context, req.StartAddr, req.NumOfReg, data)
+		}
+		if ret == -1 {
+			return nil, fmt.Errorf("fail to read register. ret=%v", ret)
+		}
+
+		values := make([]*modbus.ReadRegReply_RegisterValue, 0)
+		for i, v := range data {
+			if i >= int(req.NumOfReg) {
+				break
+			}
+			addr := i + int(req.StartAddr)
+			log.Debug().Msgf("reg[%v]=%v (0x%X)\n", addr, v, v)
+			values = append(values, &modbus.ReadRegReply_RegisterValue{
+				Addr:  int32(addr),
+				Value: fmt.Sprint(v),
+			})
+		}
+
+		return &modbus.ReadRegReply{ByteCount: int32(len(values)), IsInput: req.ReadInput, Values: values}, nil
 	}
 
-	return &modbus.ReadRegReply{ByteCount: int32(len(values)), IsInput: req.ReadInput, Values: values}, nil
+	err := errors.New("no modbus connection Id: " + req.ConnectionId)
+	log.Error().Err(err)
+	return nil, err
 }
 
 func (s *modbusServer) ReadCoil(ctx context.Context, req *modbus.ReadCoilRequest) (*modbus.ReadCoilReply, error) {
-	var ret int32
-	var data = make([]byte, 65535)
-	ret = libmodbus.SetSlave(s.connection.Context, req.SlaveAddr)
-	if ret == -1 {
-		return nil, fmt.Errorf("fail to set slave addr. ret=%v", ret)
-	}
-	if req.ReadInput {
-		ret = libmodbus.ReadInputBits(s.connection.Context, req.StartAddr, req.NumOfCoil, data)
-	} else {
-		ret = libmodbus.ReadBits(s.connection.Context, req.StartAddr, req.NumOfCoil, data)
-	}
-	if ret == -1 {
-		return nil, fmt.Errorf("fail to read coil. ret=%v", ret)
-	}
-
-	values := make([]*modbus.ReadCoilReply_CoilValue, 0)
-	for i, v := range data {
-
-		if i >= int(req.NumOfCoil) {
-			break
+	if connection, ok := s.connections[req.ConnectionId]; ok {
+		var ret int32
+		var data = make([]byte, 65535)
+		ret = libmodbus.SetSlave(connection.Context, req.SlaveAddr)
+		if ret == -1 {
+			return nil, fmt.Errorf("fail to set slave addr. ret=%v", ret)
 		}
-		addr := i + int(req.StartAddr)
-		log.Debug().Msgf("coil[%v]=%v (0x%X)\n", addr, v, v)
-		values = append(values, &modbus.ReadCoilReply_CoilValue{
-			Addr:  int32(addr),
-			Value: fmt.Sprint(v),
-		})
+		if req.ReadInput {
+			ret = libmodbus.ReadInputBits(connection.Context, req.StartAddr, req.NumOfCoil, data)
+		} else {
+			ret = libmodbus.ReadBits(connection.Context, req.StartAddr, req.NumOfCoil, data)
+		}
+		if ret == -1 {
+			return nil, fmt.Errorf("fail to read coil. ret=%v", ret)
+		}
+
+		values := make([]*modbus.ReadCoilReply_CoilValue, 0)
+		for i, v := range data {
+
+			if i >= int(req.NumOfCoil) {
+				break
+			}
+			addr := i + int(req.StartAddr)
+			log.Debug().Msgf("coil[%v]=%v (0x%X)\n", addr, v, v)
+			values = append(values, &modbus.ReadCoilReply_CoilValue{
+				Addr:  int32(addr),
+				Value: fmt.Sprint(v),
+			})
+		}
+
+		return &modbus.ReadCoilReply{BitCount: int32(len(values)), Values: values}, nil
 	}
-	return &modbus.ReadCoilReply{BitCount: int32(len(values)), Values: values}, nil
+
+	err := errors.New("no modbus connection Id: " + req.ConnectionId)
+	log.Error().Err(err)
+	return nil, err
 }
 
 func (s *modbusServer) OpenTCPConnection(ctx context.Context, req *modbus.OpenTCPConnectionRequest) (*modbus.
@@ -114,30 +128,31 @@ func (s *modbusServer) OpenTCPConnection(ctx context.Context, req *modbus.OpenTC
 	if err != nil {
 		return nil, err
 	}
-	s.connection = &modbusConnection{
+	connection := &modbusConnection{
 		Context: []libmodbus.T{*mctx},
 		Id:      fmt.Sprint(id),
 		Props:   props,
 		Type:    modbus.ConnectionStatusReply_TCP,
 	}
 
-	reply.Id = s.connection.Id
-	reply.Props = s.connection.Props
-	reply.Type = s.connection.Type
-	reply.Errmsg = s.connection.ErrorMsg
+	reply.Id = connection.Id
+	reply.Props = connection.Props
+	reply.Type = connection.Type
+	reply.Errmsg = connection.ErrorMsg
 
-	rcode := libmodbus.Connect(s.connection.Context)
+	rcode := libmodbus.Connect(connection.Context)
 
 	if rcode == -1 {
 		errstr := libmodbus.GetErrnoStr()
-		s.connection.ErrorMsg = fmt.Sprint(errstr)
-		s.connection.Status = modbus.ConnectionStatusReply_ERROR
+		connection.ErrorMsg = fmt.Sprint(errstr)
+		connection.Status = modbus.ConnectionStatusReply_ERROR
 		return reply, fmt.Errorf("Open Connection failed: %s\n", errstr)
 	} else {
-		s.connection.Status = modbus.ConnectionStatusReply_READY
+		connection.Status = modbus.ConnectionStatusReply_READY
 	}
-	reply.Status = s.connection.Status
+	reply.Status = connection.Status
 
+	s.connections[connection.Id] = connection
 	return reply, nil
 }
 
@@ -148,32 +163,43 @@ func (s *modbusServer) OpenSerialConnection(ctx context.Context, req *modbus.Ope
 
 func (s *modbusServer) CloseConnection(ctx context.Context, req *modbus.CloseConnectionRequest) (*modbus.
 	ConnectionStatusReply, error) {
-	if s.connection.Context != nil {
-		if s.connection.Type == modbus.ConnectionStatusReply_TCP {
-			libmodbus.Close(s.connection.Context)
+	if connection, ok := s.connections[req.Id]; ok {
+		if connection.Context != nil {
+			if connection.Type == modbus.ConnectionStatusReply_TCP {
+				libmodbus.Close(connection.Context)
+			}
+			libmodbus.Free(connection.Context)
+			connection.Status = modbus.ConnectionStatusReply_CLOSED
 		}
-		libmodbus.Free(s.connection.Context)
-		s.connection.Status = modbus.ConnectionStatusReply_CLOSED
+		return &modbus.ConnectionStatusReply{
+			Id:     connection.Id,
+			Props:  connection.Props,
+			Type:   connection.Type,
+			Status: connection.Status,
+			Errmsg: connection.ErrorMsg,
+		}, nil
 	}
-	return &modbus.ConnectionStatusReply{
-		Id:     s.connection.Id,
-		Props:  s.connection.Props,
-		Type:   s.connection.Type,
-		Status: s.connection.Status,
-		Errmsg: s.connection.ErrorMsg,
-	}, nil
 
+	err := errors.New("no modbus connection Id: " + req.Id)
+	log.Error().Err(err)
+	return &modbus.ConnectionStatusReply{}, err
 }
 
 func (s *modbusServer) GetConnectionStatus(ctx context.Context, req *modbus.GetConnectionStatusRequest) (*modbus.
 	ConnectionStatusReply, error) {
-	return &modbus.ConnectionStatusReply{
-		Id:     s.connection.Id,
-		Props:  s.connection.Props,
-		Type:   s.connection.Type,
-		Status: s.connection.Status,
-		Errmsg: s.connection.ErrorMsg,
-	}, nil
+	if connection, ok := s.connections[req.Id]; ok {
+		return &modbus.ConnectionStatusReply{
+			Id:     connection.Id,
+			Props:  connection.Props,
+			Type:   connection.Type,
+			Status: connection.Status,
+			Errmsg: connection.ErrorMsg,
+		}, nil
+	}
+
+	err := errors.New("no modbus connection Id: " + req.Id)
+	log.Error().Err(err)
+	return &modbus.ConnectionStatusReply{}, err
 }
 
 func (s *modbusServer) Run() {
@@ -211,8 +237,10 @@ func (s *modbusServer) Run() {
 }
 
 func (s *modbusServer) Shutdown() {
-	if s.connection.Context != nil {
-		libmodbus.Close(s.connection.Context)
-		libmodbus.Free(s.connection.Context)
+	for _, connection := range s.connections {
+		if connection.Context != nil {
+			libmodbus.Close(connection.Context)
+			libmodbus.Free(connection.Context)
+		}
 	}
 }
