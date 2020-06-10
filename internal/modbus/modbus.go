791ef11557420bed89f9bf50c9202c81b3ea7d36
Eddy Kuwana <eddy.kuwana@hitachivantara.com>
2020-06-09 23:39:57 -0700

    Fix crash and cleanup multiple connections - do not 'Free' upon 'Close' connection that cause crashing - periodic cleanup of multiple connections to modbus device/s based on maxConnIdleTimeInMins - replace sonyflake.NextID with uuid.New when generating unique Id, since sonyflake could potentially produce conflicting Ids (depending on timing)

--- internal/modbus/modbus.go
+++ internal/modbus/modbus.go
@@ -4,35 +4,38 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/google/uuid"
 	"net"
 	"os"
 	"os/signal"
 	"strconv"
+	"time"
 
 	modbus "github.com/hitachi-vantara-edge/modbus-service/api"
 	"github.com/hitachi-vantara-edge/modbus-service/pkg/libmodbus"
 	"github.com/rs/zerolog/log"
-	"github.com/sony/sonyflake"
 	"google.golang.org/grpc"
 )
 
 const gRPCServerPort = 10000
 
 type modbusServer struct {
-	connections map[string]*modbusConnection
+	connections                    map[string]*modbusConnection
+	maxConnectionIdleTimeInMinutes int
 }
 
 type modbusConnection struct {
-	Context  []libmodbus.T
-	Id       string
-	Props    map[string]string
-	Type     modbus.ConnectionStatusReply_Type
-	Status   modbus.ConnectionStatusReply_Status
-	ErrorMsg string
+	Context               []libmodbus.T
+	Id                    string
+	Props                 map[string]string
+	Type                  modbus.ConnectionStatusReply_Type
+	Status                modbus.ConnectionStatusReply_Status
+	ErrorMsg              string
+	LastSuccessfulRequest time.Time
 }
 
-func NewModbusServer(addr string, port int) *modbusServer {
-	return &modbusServer{connections: map[string]*modbusConnection{}}
+func NewModbusServer(addr string, port int, maxConnIdleTimeInMins int) *modbusServer {
+	return &modbusServer{connections: map[string]*modbusConnection{}, maxConnectionIdleTimeInMinutes: maxConnIdleTimeInMins}
 }
 
 func (s *modbusServer) ReadReg(ctx context.Context, req *modbus.ReadRegRequest) (*modbus.
@@ -52,6 +55,7 @@ func (s *modbusServer) ReadReg(ctx context.Context, req *modbus.ReadRegRequest) 
 		if ret == -1 {
 			return nil, fmt.Errorf("fail to read register. ret=%v", ret)
 		}
+		connection.LastSuccessfulRequest = time.Now()
 
 		values := make([]*modbus.ReadRegReply_RegisterValue, 0)
 		for i, v := range data {
@@ -90,6 +94,7 @@ func (s *modbusServer) ReadCoil(ctx context.Context, req *modbus.ReadCoilRequest
 		if ret == -1 {
 			return nil, fmt.Errorf("fail to read coil. ret=%v", ret)
 		}
+		connection.LastSuccessfulRequest = time.Now()
 
 		values := make([]*modbus.ReadCoilReply_CoilValue, 0)
 		for i, v := range data {
@@ -115,19 +120,16 @@ func (s *modbusServer) ReadCoil(ctx context.Context, req *modbus.ReadCoilRequest
 
 func (s *modbusServer) OpenTCPConnection(ctx context.Context, req *modbus.OpenTCPConnectionRequest) (*modbus.
 	ConnectionStatusReply, error) {
+	log.Info().Msgf("OpenTCPConnection, address: %s, port: %v", req.Addr, req.Port)
 
 	mctx := libmodbus.NewTcp(req.Addr, req.Port)
 
 	reply := &modbus.ConnectionStatusReply{}
 
-	sf := sonyflake.NewSonyflake(sonyflake.Settings{})
 	props := make(map[string]string)
 	props["addr"] = req.Addr
 	props["port"] = strconv.Itoa(int(req.Port))
-	id, err := sf.NextID()
-	if err != nil {
-		return nil, err
-	}
+	id := uuid.New()
 	connection := &modbusConnection{
 		Context: []libmodbus.T{*mctx},
 		Id:      fmt.Sprint(id),
@@ -149,6 +151,8 @@ func (s *modbusServer) OpenTCPConnection(ctx context.Context, req *modbus.OpenTC
 		return reply, fmt.Errorf("Open Connection failed: %s\n", errstr)
 	} else {
 		connection.Status = modbus.ConnectionStatusReply_READY
+		connection.LastSuccessfulRequest = time.Now()
+		log.Info().Msgf("OpenTCPConnection succeed, address: %s, port: %v, connectionId: %s", req.Addr, req.Port, connection.Id)
 	}
 	reply.Status = connection.Status
 
@@ -161,16 +165,32 @@ func (s *modbusServer) OpenSerialConnection(ctx context.Context, req *modbus.Ope
 	return &modbus.ConnectionStatusReply{}, nil
 }
 
+//ToDo: revisit workaround
+//Note: Calling 'Free' from modbus.go after 'Close' caused exception/crash
+/*
+	//*** Error in `./main': free(): invalid size: 0x000000c4200a5d40 ***
+	//
+	//SIGABRT: abort
+	//PC=0x7f6a13343fff m=11 sigcode=18446744073709551610
+	//
+	//goroutine 0 [idle]:
+	//runtime: unknown pc 0x7f6a13343ff
+*/
+//- Tried updating libmodbus to support a new method: libmodbus.CloseAndFree (i.e. modbus_close_and_free in modbus.c) to do both modbus_close and modbus_free simultaneously, did not resolve the crash issue
+//- Workaround for now: do not 'Free' - need to further investigate potential memory leak
 func (s *modbusServer) CloseConnection(ctx context.Context, req *modbus.CloseConnectionRequest) (*modbus.
 	ConnectionStatusReply, error) {
+	log.Info().Msgf("CloseConnection, connectionId: %s", req.Id)
 	if connection, ok := s.connections[req.Id]; ok {
 		if connection.Context != nil {
 			if connection.Type == modbus.ConnectionStatusReply_TCP {
 				libmodbus.Close(connection.Context)
 			}
-			libmodbus.Free(connection.Context)
+			//libmodbus.Free(connection.Context)
 			connection.Status = modbus.ConnectionStatusReply_CLOSED
+			log.Info().Msgf("CloseConnection succeed, connectionId: %s", req.Id)
 		}
+		delete(s.connections, req.Id)
 		return &modbus.ConnectionStatusReply{
 			Id:     connection.Id,
 			Props:  connection.Props,
@@ -221,6 +241,31 @@ func (s *modbusServer) Run() {
 		}
 	}()
 
+	// Periodic cleanup of modbus connection to device, e.g. in case of modbus-adapter crashed/restarted hence could not close connections properly by making CloseConnection calls to this modbus-service
+	var ticker *time.Ticker
+	var done chan bool
+	if s.maxConnectionIdleTimeInMinutes > 0 {
+		maxIdleDuration := time.Duration(s.maxConnectionIdleTimeInMinutes) * time.Minute
+		ticker = time.NewTicker(maxIdleDuration)
+		done = make(chan bool)
+
+		go func() {
+			for {
+				select {
+				case <-done:
+					return
+				case t := <-ticker.C:
+					for _, connection := range s.connections {
+						if t.Sub(connection.LastSuccessfulRequest) > maxIdleDuration {
+							log.Info().Msgf("Closing idle connection, connectionId: %s, lastSuccessfulRequest: %+v, currentTickTime: %+v, maxConnectionIdleTimeInMinutes: %+v", connection.Id, connection.LastSuccessfulRequest, t, s.maxConnectionIdleTimeInMinutes)
+							s.CloseConnection(context.Background(), &modbus.CloseConnectionRequest{Id: connection.Id})
+						}
+					}
+				}
+			}
+		}()
+	}
+
 	c := make(chan os.Signal, 1)
 
 	signal.Notify(c, os.Interrupt)
@@ -229,6 +274,11 @@ func (s *modbusServer) Run() {
 
 	log.Info().Msgf("Stopping gRPC Server")
 	grpcServer.GracefulStop()
+
+	if ticker != nil {
+		ticker.Stop()
+		done <- true
+	}
 
 	log.Info().Msgf("Shutting down Modbus Library Service")
 	s.Shutdown()
@@ -240,7 +290,7 @@ func (s *modbusServer) Shutdown() {
 	for _, connection := range s.connections {
 		if connection.Context != nil {
 			libmodbus.Close(connection.Context)
-			libmodbus.Free(connection.Context)
+			//libmodbus.Free(connection.Context)
 		}
 	}
 }
